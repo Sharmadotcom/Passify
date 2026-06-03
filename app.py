e52fc77325d13ff870f786209090f5d781b66bc6
Sharmadotcom <sharmasarvesh390@gmail.com>
2026-06-04 00:29:14 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,168 @@
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    session,
+    url_for
+)
+import sqlite3
+from werkzeug.security import (
+    generate_password_hash,
+    check_password_hash
+)
+from database import init_db
+
+init_db()
+app = Flask(__name__)
+app = Flask(__name__)
+
+app.secret_key = "super-secret-key"
+@app.route("/", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE username = ?",
+            (username,)
+        )
+
+        user = cursor.fetchone()
+
+        conn.close()
+
+        if user:
+
+            stored_hash = user[2]
+
+            if check_password_hash(
+                stored_hash,
+                password
+            ):
+
+                session["user_id"] = user[0]
+                session["username"] = user[1]
+
+                return redirect("/dashboard")
+
+        return "Invalid Username or Password"
+
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        password_hash = generate_password_hash(password)
+
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        try:
+
+            cursor.execute(
+                """
+                INSERT INTO users
+                (username, password_hash)
+                VALUES (?, ?)
+                """,
+                (username, password_hash)
+            )
+
+            conn.commit()
+            conn.close()
+
+            return "User Registered Successfully"
+
+        except sqlite3.IntegrityError:
+
+            conn.close()
+            return "User Already Exists"
+
+    return render_template("register.html")
+
+@app.route("/dashboard")
+def dashboard():
+
+    if "user_id" not in session:
+        return redirect("/")
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        SELECT id,
+               website,
+               site_username
+        FROM vault
+        WHERE user_id = ?
+        """,
+        (session["user_id"],)
+    )
+
+    passwords = cursor.fetchall()
+
+    conn.close()
+
+    return render_template(
+        "dashboard.html",
+        passwords=passwords
+    )
+@app.route("/logout")
+def logout():
+
+    session.clear()
+
+    return redirect("/")
+@app.route("/add-password", methods=["GET", "POST"])
+def add_password():
+
+    if "user_id" not in session:
+        return redirect("/")
+
+    if request.method == "POST":
+
+        website = request.form["website"]
+        site_username = request.form["site_username"]
+        site_password = request.form["site_password"]
+
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            INSERT INTO vault
+            (website, site_username, site_password, user_id)
+            VALUES (?, ?, ?, ?)
+            """,
+            (
+                website,
+                site_username,
+                site_password,
+                session["user_id"]
+            )
+        )
+
+        conn.commit()
+        conn.close()
+
+        return redirect("/dashboard")
+
+    return render_template("add_password.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
---
+++ database.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        password_hash TEXT NOT NULL
+    )
+    """)
+    cursor.execute("""
+CREATE TABLE IF NOT EXISTS vault(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    website TEXT NOT NULL,
+    site_username TEXT NOT NULL,
+    site_password TEXT NOT NULL,
+    user_id INTEGER NOT NULL
+)
+""")
+
+    conn.commit()
+    conn.close()
